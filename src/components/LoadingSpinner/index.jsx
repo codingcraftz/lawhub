22c6b282bab5bf86074303f95d53ad586fc1c8ea
zoonyoung <9851248@gmail.com>
2024-10-31 10:40:48 +0900

    refactor: class to className

--- src/components/LoadingSpinner/index.jsx
+++ src/components/LoadingSpinner/index.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 const LoadingSpinner = () => {
   return (
     <div className="flex w-screen h-screen justify-center items-center bg-black-50 opacity-80">
-      <div class="lds-spinner">
+      <div className="lds-spinner">
         <div></div>
         <div></div>
         <div></div>
