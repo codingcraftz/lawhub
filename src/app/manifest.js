4e7d56b2b1677837b66e21700dc255c9c8f11dd1
zoonyoung <9851248@gmail.com>
2024-11-11 15:28:53 +0900

    refactor: Modify icons파일명 수정

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -6,12 +6,12 @@ export default function manifest() {
     background_color: "#f5f5f5",
     icons: [
       {
-        src: "LawHub_Logo_192.png",
+        src: "/LawHub_Logo_192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "LawHub_Logo_512.png",
+        src: "/LawHub_Logo_512.png",
         sizes: "512x512",
         type: "image/png",
       },
