3112f07b3450114f8e92beab7e96c4f30688f25e
zoonyoung <9851248@gmail.com>
2024-11-11 18:45:46 +0900

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
-import withPWA from "next-pwa";
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const isProd = process.env.NODE_ENV === "production";
+const withPWA = withPWAInit({
+  dest: "public",
+});
 
 export default withPWA({
-  dest: "public", // 빌드 후 생성된 PWA 파일이 저장될 위치
-  disable: !isProd, // 개발 환경에서는 PWA 비활성화
-  register: true,
-  skipWaiting: true,
-  reactStrictMode: true,
+  // Your Next.js config
 });
--- src/app/layout.js
+++ src/app/layout.js
@@ -15,19 +15,54 @@ const nanumGothic = Nanum_Gothic({
   weight: ["400", "700"],
 });
 
+const APP_NAME = "PWA App";
+const APP_DEFAULT_TITLE = "My Awesome PWA App";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Best PWA app in the world!";
+
 export const metadata = {
-  title: "LawHub",
-  description: "Legal management service",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <head>
-        <meta name="theme-color" content="#000000" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="mobile-web-app-status-bar-style" content="default" />
-      </head>
       <body className={`${nanumGothic.className} root-portal`}>
         <UserProvider>
           <ThemeProvider attribute="class">
