b144e4c9ea5f78ed6e634174104bc1eaf292c8f0
zoonyoung <9851248@gmail.com>
2025-01-17 17:02:52 +0900

    style

--- src/app/client/assignment/_components/DynamicSelect.jsx
+++ src/app/client/assignment/_components/DynamicSelect.jsx
@@ -5,17 +5,11 @@ import React from "react";
 const DynamicSelect = ({ options, value, onChange, placeholder, label }) => {
 	return (
 		<Box className="flex-1">
-			<Text className="font-medium text-gray-12 mb-1">{label}</Text>
+			<Text size="2" color="gray" className="mb-1">
+				{label}</Text>
 			<select
 				value={value}
 				onChange={(e) => onChange(e.target.value)}
-				className="
-          w-full p-2
-          border border-gray-6
-          rounded text-gray-12
-          focus:outline-none focus:border-gray-8
-          bg-gray-1
-        "
 			>
 				<option value="">{placeholder}</option>
 				{options.map((option) => (
